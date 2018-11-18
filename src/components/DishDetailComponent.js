5479cb5e723dc4b52ea0536591dd7a9ce26ba972
thanhvk <thanhvk.w3s@gmail.com>
2018-11-18 19:26:44 +0700

    Presentational and Container Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -14,25 +14,27 @@ class DishDetail extends Component {
                 return (
                     <div>
                         <p>{ comment.comment }</p>
-                        <p>-- { comment.author }, { comment.date }</p>
+                        <p>-- { comment.author }, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                     </div>
                 );
             });
 
             return (
-                <div className="row"> 
-                    <div  className="col-md-5 m-1">
-                        <Card>
-                            <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
-                            <CardBody>
-                            <CardTitle>{this.props.dish.name}</CardTitle>
-                            <CardText>{this.props.dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div  className="col-md-5 m-1">
-                        <h5>Comment</h5>
-                        { comments }
+                <div className="container">
+                    <div className="row"> 
+                        <div  className="col-md-5 m-1">
+                            <Card>
+                                <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+                                <CardBody>
+                                <CardTitle>{this.props.dish.name}</CardTitle>
+                                <CardText>{this.props.dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div  className="col-md-5 m-1">
+                            <h5>Comment</h5>
+                            { comments }
+                        </div>
                     </div>
                 </div>
             );
